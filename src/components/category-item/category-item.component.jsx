d4dd2de383d9979a5b7c65b5b1430a50a7788c4e
vinh pham <hungduonggia181@gmail.com>
2022-09-27 15:48:56 -0400

    categories

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./category-item.styles.scss";
+
+function CategoryItem({ category }) {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
